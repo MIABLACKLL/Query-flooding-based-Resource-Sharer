7cb06730490415ac441a1b0ae43238066ab53bb3
MIABLACKLL <328381256@qq.com>
2019-10-16 15:12:37 +0800

    create

--- CSnetworkProject/FileManagement.h
+++ CSnetworkProject/FileManagement.h
@@ -1,1 +1,14 @@
 #pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+class CFileMnagement
+{
+public:
+	CFileMnagement() = default;
+	~CFileMnagement() = default;
+
+
+private:
+	string m_CurrentPath;
+};
--- CSnetworkProject/main.cpp
+++ CSnetworkProject/main.cpp
@@ -6,37 +6,40 @@
 
 constexpr auto BUF_SIZE = 128;
 
+//int test()
+//{
+//	WSADATA WSAData;
+//	WSAStartup(MAKEWORD(2, 2), &WSAData);
+//
+//	SOCKET ServerSock = socket(AF_INET, SOCK_STREAM, 0);
+//	
+//	sockaddr_in SockAddr;
+//	memset(&SockAddr, 0, sizeof(SockAddr));
+//	SockAddr.sin_family = AF_INET;
+//	InetPton(AF_INET,"127.0.0.1",&SockAddr.sin_addr);
+//	SockAddr.sin_port = htons(9999);
+//	bind(ServerSock, reinterpret_cast<sockaddr*>(&SockAddr), sizeof(SockAddr));
+//
+//	listen(ServerSock, 9999);
+//
+//	SOCKADDR ClientAddr;
+//	int SockAddrSize = sizeof SOCKADDR;
+//	SOCKET ClientSock = accept(ServerSock, reinterpret_cast<sockaddr*>(&ClientAddr), &SockAddrSize);
+//	std::string Buffer;
+//	int StrLen = recv(ClientSock,const_cast<char *>(Buffer.c_str()), BUF_SIZE, 0);
+//	std::cout << Buffer.c_str();
+//	send(ClientSock, const_cast<char *>(Buffer.c_str()), StrLen, 0);
+//
+//	closesocket(ClientSock);
+//	closesocket(ServerSock);
+//
+//	WSACleanup();
+//
+//	system("pause");
+//
+//}
+
 int main()
 {
-	WSADATA WSAData;
-	WSAStartup(MAKEWORD(2, 2), &WSAData);
-
-	SOCKET ServerSock = socket(AF_INET, SOCK_STREAM, 0);
-	
-	sockaddr_in SockAddr;
-	memset(&SockAddr, 0, sizeof(SockAddr));
-	SockAddr.sin_family = AF_INET;
-	InetPton(AF_INET,"127.0.0.1",&SockAddr.sin_addr);
-	SockAddr.sin_port = htons(9999);
-	bind(ServerSock, reinterpret_cast<sockaddr*>(&SockAddr), sizeof(SockAddr));
-
-	listen(ServerSock, 9999);
-
-	SOCKADDR ClientAddr;
-	int SockAddrSize = sizeof SOCKADDR;
-	SOCKET ClientSock = accept(ServerSock, reinterpret_cast<sockaddr*>(&ClientAddr), &SockAddrSize);
-	std::string Buffer;
-	int StrLen = recv(ClientSock,const_cast<char *>(Buffer.c_str()), BUF_SIZE, 0);
-	std::cout << Buffer.c_str();
-	send(ClientSock, const_cast<char *>(Buffer.c_str()), StrLen, 0);
-
-	closesocket(ClientSock);
-	closesocket(ServerSock);
-
-	WSACleanup();
-
-	system("pause");
-
-
 
 }
